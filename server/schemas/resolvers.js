648a8a40ae21ae24bca761f181583e01f6dc1c1a
Mark Elliott <markelliottva@gmail.com>
2022-07-11 13:33:20 -0400

    initial commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+  Query: {
+    thoughts: async () => {
+      return Thought.find().sort({ createdAt: -1 });
+    },
+  },
+};
