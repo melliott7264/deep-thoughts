58832b8b90622cb6a49e64be8bd65462842a469f
Mark Elliott <markelliottva@gmail.com>
2022-07-14 20:34:20 -0400

    reloaded everything from snapshot

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,22 +1,21 @@
-const { runHttpQuery } = require('apollo-server-core');
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-  authMiddleware: function ({ req }) {
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (req.headers.authroization) {
-      token = token.split(' ').pop().trim();
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
-    console.log(token);
+
     if (!token) {
       return req;
     }
@@ -30,4 +29,9 @@ module.exports = {
 
     return req;
   },
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  }
 };
